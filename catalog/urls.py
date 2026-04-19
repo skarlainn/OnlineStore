cfbff14a04aa355538cdf8861e2f8fb06d0281f1
Diana <ddmitrieva098@example.com>
2026-04-19 15:25:37 +1100

    correction

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
+from . import views
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home
-
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
-]
+    path('home/', views.home_view, name='home'),
+    path('contacts/', views.contacts_view, name='contacts'),
+    ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request, template_name='home.html')
+def home_view(request):
+    return render(request, 'home.html')
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
+def contacts_view(request):
+    return render(request, 'contacts.html')
 
-        return HttpResponse(f"Спаисбо за обращение, {name}!")
 
-    return render(request, template_name='contacts.html')
+def contact(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+        print(name)
+        print(message)
+        # Здесь мы просто возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+    return render(request, 'contact.html')
