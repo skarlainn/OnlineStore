4bed7aac2cb739edeeaa14ef65d2fdaf8cb1e191
Diana <ddmitrieva098@example.com>
2026-04-28 20:52:00 +1100

    create files

--- confiig/settings.py
+++ confiig/settings.py
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
